e8a46c500df69a5367c1e13be6175e6341cd5d09
yashasjoshi <yashasjoshi1996@gmail.com>
2022-04-26 19:02:59 +0530

    Build - 1

---
+++ build/precache-manifest.453e0aaff1f05cbce57bd102a707e6e2.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb2a07688ec20258fba07794e38e2d5",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "2d3136565abdc7a615ad",
+    "url": "/website/static/css/main.461c6ff9.chunk.css"
+  },
+  {
+    "revision": "de08fd08c11b73f437eb",
+    "url": "/website/static/js/2.fe806c71.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/website/static/js/2.fe806c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3136565abdc7a615ad",
+    "url": "/website/static/js/main.9bbada19.chunk.js"
+  },
+  {
+    "revision": "2a6f0003a8d2de8301ca",
+    "url": "/website/static/js/runtime-main.0af46ba4.js"
+  },
+  {
+    "revision": "cfe2e983663a034838540626eae9c2f6",
+    "url": "/website/static/media/Logo.cfe2e983.png"
+  },
+  {
+    "revision": "5994da671aad2d63cf2bcaae2ee462a8",
+    "url": "/website/static/media/Wallet.5994da67.svg"
+  },
+  {
+    "revision": "0a6f52a8ec175a29c552555a66f35899",
+    "url": "/website/static/media/auction.0a6f52a8.svg"
+  },
+  {
+    "revision": "5920b9b3db93f11a61c9849ef7a53f8e",
+    "url": "/website/static/media/beneficiaries.5920b9b3.svg"
+  },
+  {
+    "revision": "f1d3fa72434ebf91e785de2083f3738c",
+    "url": "/website/static/media/building.f1d3fa72.svg"
+  },
+  {
+    "revision": "85c06004483b201d25906db9d1cbe739",
+    "url": "/website/static/media/ethereum.85c06004.svg"
+  },
+  {
+    "revision": "ea01ed4580c02a3b5635de25f91e256e",
+    "url": "/website/static/media/fraud.ea01ed45.svg"
+  },
+  {
+    "revision": "114e2b591cf56452fa30a06e6b8dc207",
+    "url": "/website/static/media/illus-brackets.114e2b59.svg"
+  },
+  {
+    "revision": "ee71105b7cefe7ee14f8c7db3b69fb00",
+    "url": "/website/static/media/illus-planet-waves.ee71105b.svg"
+  },
+  {
+    "revision": "2889538e879c7e78e1abac8ffaed91e8",
+    "url": "/website/static/media/illus-smile.2889538e.svg"
+  },
+  {
+    "revision": "376a788a40e6dbf031023e5df9036679",
+    "url": "/website/static/media/illus-snowflake.376a788a.svg"
+  },
+  {
+    "revision": "42b3938201f084210e0c5334d1562c95",
+    "url": "/website/static/media/illus-star.42b39382.svg"
+  },
+  {
+    "revision": "4a2f2af8170447ae8160bd82b84786dc",
+    "url": "/website/static/media/illus-sun.4a2f2af8.svg"
+  },
+  {
+    "revision": "6407227ab10b41139460af61e531e64e",
+    "url": "/website/static/media/illus-thumb.6407227a.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/website/static/media/illus-top-right-waves.ea694903.svg"
+  },
+  {
+    "revision": "47fa4f95b7e97a72785c57b282eb18f2",
+    "url": "/website/static/media/illustration-closed-waves.47fa4f95.svg"
+  },
+  {
+    "revision": "0ae73744f06a7f5864401f37d0960f69",
+    "url": "/website/static/media/mint.0ae73744.svg"
+  },
+  {
+    "revision": "b19402dc566bddc1872304d020ba68ec",
+    "url": "/website/static/media/portfolio.b19402dc.svg"
+  },
+  {
+    "revision": "b8955989462f2dcd50f7839ac9dc33cb",
+    "url": "/website/static/media/responsibility.b8955989.svg"
+  },
+  {
+    "revision": "c1a997f1d1f5c900ee2cd2b9c73040f7",
+    "url": "/website/static/media/security.c1a997f1.svg"
+  },
+  {
+    "revision": "ba74e61a3fab1f42f2fe9bda18a5cdee",
+    "url": "/website/static/media/token-standards.ba74e61a.svg"
+  },
+  {
+    "revision": "1d4cad7f65cc403ac4072341209b22d1",
+    "url": "/website/static/media/touch.1d4cad7f.svg"
+  },
+  {
+    "revision": "60a29c0a505e298f8bc6d5ae1df57968",
+    "url": "/website/static/media/trustees.60a29c0a.svg"
+  }
+]);
